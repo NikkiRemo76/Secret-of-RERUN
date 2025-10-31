e56c3a75dca30cf582177e192ac1000171a63993
Nova <mesterdani210@gmail.com>
2025-10-31 14:15:30 +0200

    заставил transitioning работать и улучшил  код и текст

--- data/states/rerun/UpdateScreenState.hx
+++ data/states/rerun/UpdateScreenState.hx
@@ -1,27 +1,29 @@
-
+var transitioning:Bool = false;
 function create() {
-    warnText = new FlxText(0, 0, FlxG.width,
-        "Sup bro, looks like you're running an\noutdated version of the mod,\n
-        Press ACCEPT to download the newest version,\nPress BACK to proceed anyway.\n
-        \n
-        Thank you for playing our mod!",
-        32);
+    // не переноси строки он это чувствует
+    warnText = new FlxText(0, 0, FlxG.width,"Sup bro, looks like you're running an\noutdated version of the mod,\n\nPress ACCEPT to download the newest version,\nPress BACK to proceed anyway.\n\nThank you for playing our mod!");
     warnText.setFormat(null, 32, FlxColor.WHITE, 'center');
     warnText.screenCenter();
-    warnText.y += 80;
+    warnText.y += 20;
     add(warnText);
 }
 
-function update(elapsed:Float) {
-    
-    if (controls.ACCEPT || controls.BACK) {
+function update() { //если не используешь elapsed, не пиши его
+    if (!transitioning && controls.ACCEPT) {
         transitioning = true;
 		FlxG.camera.flash(0xff0000, 1);
-		CoolUtil.playMenuSFX("1");
-        if(controls.ACCEPT){
-            FlxG.openURL("https://github.com/NikkiRemo76/Secret-of-RERUN/releases", "_blank");
-        }
-        FlxTween.tween(warnText, {alpha: 0}, 1, {ease: FlxEase.backOut});
-		new FlxTimer().start(1.2, (_) -> FlxG.switchState(new TitleState()));
-	}
+        CoolUtil.playMenuSFX("1");
+        FlxTween.tween(warnText, {alpha: 0}, 1, {ease: FlxEase.backOut, onComplete: function(_) {
+            CoolUtil.openURL("https://github.com/NikkiRemo76/Secret-of-RERUN/releases");
+            FlxG.switchState(new TitleState());
+        }});
+    }
+    if (!transitioning && controls.BACK) {
+        transitioning = true;
+        FlxG.camera.flash(0xff0000, 1);
+        CoolUtil.playMenuSFX("1");
+        FlxTween.tween(warnText, {alpha: 0}, 1, {ease: FlxEase.backOut, onComplete: function(_) {
+            FlxG.switchState(new TitleState());
+        }});
+    }
 }
--- data/states/rerun/Warn.hx
+++ data/states/rerun/Warn.hx
@@ -1,5 +1,8 @@
-
 import funkin.backend.utils.HttpUtil;
+
+var transitioning:Bool = false;
+var curModVersion:String = 'Alpha 0 (Fleatway)'; //MAKE SURE TO CHANGE THIS EVERY UPDATE
+var curWebVersion:String = HttpUtil.requestText('https://gist.githubusercontent.com/NikkiRemo76/091b6d078ada192b12b1ce8b1bea599c/raw/416d59b137f2e80f13d8b891cd60a223d0e9ac3a/secret%2520of%2520rerun%2520version');
 
 function create() {
     warn = new FlxSprite().loadGraphic(Paths.image('menus/warning'));
@@ -9,17 +12,15 @@ function create() {
 	add(warn);
 }
 
-var curModVersion:String = 'Alpha 1 (Fleatway)'; //MAKE SURE TO CHANGE THIS EVERY UPDATE
-var curWebVersion:String = HttpUtil.requestText('https://gist.githubusercontent.com/NikkiRemo76/091b6d078ada192b12b1ce8b1bea599c/raw/416d59b137f2e80f13d8b891cd60a223d0e9ac3a/secret%2520of%2520rerun%2520version');
 function update(elapsed:Float) {
-    
-    if (controls.ACCEPT) {
+    if (!transitioning controls.ACCEPT) {
 		FlxG.camera.flash(0xff0000, 1);
 		CoolUtil.playMenuSFX("1");
         FlxTween.tween(warn, {alpha: 0}, 1, {ease: FlxEase.backOut});
-        if(curModVersion != curWebVersion){
+
+        if (curModVersion != curWebVersion) {
             new FlxTimer().start(1.2, (_) -> FlxG.switchState(new ModState("rerun/UpdateScreenState")));
-        }else{
+        } else {
             new FlxTimer().start(1.2, (_) -> FlxG.switchState(new TitleState()));
         }
 	}
