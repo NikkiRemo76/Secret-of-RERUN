2d60543366e4b7d9f607b36653ec9a91495c2e70
Nova <mesterdani210@gmail.com>
2025-11-09 17:05:23 +0200

    ой езё итрвалщп

--- data/states/CreditsMain.hx
+++ data/states/CreditsMain.hx
@@ -1,3 +1,4 @@
 function postCreate() {
-    CoolUtil.playMusic(Paths.music("menus/creditsTheme"));
+    CoolUtil.playMusic(Paths.music("menus/creditsTheme"), false, 0);
+    FlxG.sound.music.fadeIn(2, 0, 0.7);
 }
